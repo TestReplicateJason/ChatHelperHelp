725a5cb796df25ffd01b44202f550a9bd9015302
TestReplicateJason <jasonreplicate@gmail.com>
2024-09-26 15:47:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function togglePanel(panelId) {
         panel.classList.add('active');
     }
 }
+
+window.addEventListener('resize', function() {
+    // Если нужно, установите фиксированную ширину
+    document.body.style.width = '100%';
+    window.scrollTo(0, 0); // Сброс позиции прокрутки
+});
+
+let lastTouchEnd = 0;
+
+document.addEventListener('touchend', function(event) {
+    const now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
+
+document.addEventListener('gesturestart', function (event) {
+    event.preventDefault(); // Запрет на масштабирование
+});
+
